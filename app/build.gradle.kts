38f4f3f6ed7cf59db37435405b98a9577b707e0f
Timothy <fynx626@gmail.com>
2024-07-16 18:18:32 +0100

    add vertex ai dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.vertexai)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
